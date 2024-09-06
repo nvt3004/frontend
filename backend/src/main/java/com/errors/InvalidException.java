9554b9a11905211f641e169e2daa8917b17e1525
nvt3004 <130243086+nvt3004@users.noreply.github.com>
2024-09-06 19:15:06 +0700

    Delete backend directory

--- backend/src/main/java/com/errors/InvalidException.java
+++
@@ -1,7 +0,0 @@
-package com.errors;
-
-public class InvalidException extends RuntimeException {
-    public InvalidException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/repositories/PaymentMethodJPA.java
+++
@@ -1,8 +0,0 @@
-package com.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.entities.PaymentMethod;
-
-public interface PaymentMethodJPA extends JpaRepository<PaymentMethod, Integer> {
-
-}
